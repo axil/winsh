6d4d20f6c806add59fa1e366f4f84dd9bf892a62
Lev Maximov <lev.maximov@gmail.com>
2020-06-08 02:28:46 +0700

    use where.exe to search for commands in path

--- sh.py
+++ sh.py
@@ -114,17 +114,20 @@ def get_rc_exc(rc):
 
 
 def which(program):
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program): return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
+    p = subp.run(['where', program], capture_output=True)
+    if p.returncode == 0:
+        return p.stdout.decode().rstrip()
+#    def is_exe(fpath):
+#        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
+#
+#    fpath, fname = os.path.split(program)
+#    if fpath:
+#        if is_exe(program): return program
+#    else:
+#        for path in os.environ["PATH"].split(os.pathsep):
+#            exe_file = os.path.join(path, program)
+#            if is_exe(exe_file):
+#                return exe_file
 
     return None
 
